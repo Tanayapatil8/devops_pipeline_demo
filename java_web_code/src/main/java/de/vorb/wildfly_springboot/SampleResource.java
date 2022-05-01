d50b9a3d8d055c6d1c4f0efdb8e6b91b28012ae5
Tanayapatil8 <67539820+Tanayapatil8@users.noreply.github.com>
2022-05-01 12:45:41 +0530

    Update SampleResource.java

--- java_web_code/src/main/java/de/vorb/wildfly_springboot/SampleResource.java
+++ java_web_code/src/main/java/de/vorb/wildfly_springboot/SampleResource.java
@@ -2,13 +2,23 @@ package de.vorb.wildfly_springboot;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.io.*
+class mul
+{
+    public static void main(String[] args)
+    {
+        int n=5;
+        for(int i=1;i<=10;i++)
+            System.out.println(n + " * " + i + " = " + n * i);
+    }    
+}    
 
-@RestController
-public class SampleResource {
+//@RestController
+//public class SampleResource {
 
-    @RequestMapping("/")
-    public String hello() {
-        return "Hello World!";
-    }
+  //  @RequestMapping("/")
+    //public String hello() {
+      //  return "Hello World!";
+    //}
 
-}
+//}
